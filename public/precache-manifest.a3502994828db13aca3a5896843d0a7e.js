24377097ac6674aeb57a572018019af488c70609
Vineet-Sharma29 <vineet.s17@iiits.in>
2019-04-05 23:52:11 +0530

    added public/precache-manifest.a3502994828db13aca3a5896843d0a7e.js

---
+++ public/precache-manifest.a3502994828db13aca3a5896843d0a7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "c70055ba465ecd4125a7",
+    "url": "/js/chunk-vendors.ad4e3e65.js"
+  },
+  {
+    "revision": "98ad0643c17d9fdb149c",
+    "url": "/js/app.7799830a.js"
+  },
+  {
+    "revision": "e16db472cd00e4dc0d5c37ba326260d0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c70055ba465ecd4125a7",
+    "url": "/css/chunk-vendors.84d7616d.css"
+  }
+];
